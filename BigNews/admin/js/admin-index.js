22ea45ab1ea0c3dff1000410e2f7c04d4bace080
zucc-wjr <1935858467@qq.com>
2019-10-09 12:18:54 +0800

    首页导航栏

--- BigNews/admin/js/admin-index.js
+++ BigNews/admin/js/admin-index.js
@@ -12,4 +12,24 @@ $(function () {
       $('.user_info>span').text('欢迎  ' + data.data.nickname);
     }
   });
+//一级列表操作
+  $('.level01').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+    // console.log($(this).next());
+
+    // 如果点击是文章管理,滑出ul,并进行初始化操作
+    if($(this).next().hasClass('level02')){
+      $(this).next().slideToggle();
+      $(this).find('b').toggleClass('rotate0');
+      $('.level02>li>a').first()[0].click();
+    } else $('.level02>li').removeClass('active');
+    
+  })
+
+  //二级列表
+  $('.level02').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+  })
+
+
 })
