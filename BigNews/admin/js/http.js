1f10b1838303f8b6c27aacc9584c8c30fa0c0069
zucc-wjr <1935858467@qq.com>
2019-10-09 09:28:32 +0800

    登录验证

---
+++ BigNews/admin/js/http.js
@@ -0,0 +1,33 @@
+/*实际开发中哪些js文件需要使用沙箱 
+不需要使用： 这个js只需要导入一次   前端三层js
+需要使用： 这个js需要多次导入  http.js 网络请求层
+*/
+
+/* 沙箱模式 */
+(function(w){
+    var baseURL = 'http://localhost:8080/api/v1'
+    var BigNew = {
+        baseURL:baseURL,//基地址
+        user_login:      baseURL + '/admin/user/login',//用户登录
+        user_info:       baseURL + '/admin/user/info',//用户信息
+        user_detail:     baseURL + '/admin/user/detail',//用户详情
+        user_edit:       baseURL + '/admin/user/edit',//用户编辑
+        category_list:   baseURL + '/admin/category/list',//文章类别查询
+        category_add:    baseURL + '/admin/category/add',//文章类别新增
+        category_search: baseURL + '/admin/category/search',//文章类别搜索
+        category_edit:   baseURL + '/admin/category/edit',//文章类别编辑
+        category_delete: baseURL + '/admin/category/delete',//文章类别删除
+        article_query:   baseURL + '/admin/article/query',//文章搜索
+        article_publish: baseURL + '/admin/article/publish',//文章发布
+        article_search:  baseURL + '/admin/article/search',//文章信息查询
+        article_edit:    baseURL + '/admin/article/edit',//文章编辑
+        article_delete:  baseURL + '/admin/article/delete',//文章删除
+        comment_list:    baseURL + '/admin/comment/search',//文章评论列表
+        comment_pass:    baseURL + '/admin/comment/pass',//文章评论通过
+        comment_reject:  baseURL + '/admin/comment/reject',//文章评论不通过
+        comment_delete:  baseURL + '/admin/comment/delete',//文章评论删除
+    };
+
+    //暴露接口
+    w.BigNew = BigNew;
+})(window);
---
+++ BigNews/admin/js/login.js
@@ -0,0 +1,40 @@
+$(function() {
+  'use strict'
+  $(".input_sub").click((e)=>{
+      e.preventDefault();
+      let username = $(".input_txt").val();
+      let password = $(".input_pass").val();
+      if(username.trim().length==0 || password.trim().length==0){
+        $('.model-body>p').text('请输入用户名和密码');
+        $('#myModal').modal();
+          return;
+      }
+      $.ajax({
+      url:'http://localhost:8080/api/v1/admin/user/login',
+      type:'post',
+      dataType:'json',
+      data:{
+        username:username,
+        password:password,
+      },
+      success: function(data){
+        if(data.code == 200){
+          // alert("登录成功");
+          localStorage.setItem('token',data.token);
+          $('.modal-body>P').text(data.msg);
+          $('#myModal').modal();
+          $('#myModal').on('hidden.bs.modal', function (e) {
+            // do something...
+            //跳转首页
+            window.location.href = './index.html';
+          });
+        } else {
+          $('.modal-body>p').text(data.msg);
+          $('#myModal').modal();
+          $(".input_pass").val('');
+        }
+      }
+      });
+  })
+  
+})
